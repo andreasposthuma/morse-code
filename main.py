3c6894173395b46c6845ef7a73ae468c809bb2e7
andreasposthuma <74873669+andreasposthuma@users.noreply.github.com>
2020-11-25 08:39:19 -0700

    finished for sprint

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
-
 import winsound
-
 MORSE_CODE = {
     "a":".-", 
-    "b":"---.",
+    "b":"-...",
+    "c":"-.-.",
+    "d":"-..",
+    "e":".",
+    "f":"..-.",
+    "g":"--.",
+    "h":"....",
+    "i":"..",
+    "j":".---",
+    "k":"-.-",
+    "l":".-..",
+    "m":"--",
+    "n":"-.",
+    "o":"---",
+    "p":".--.",
+    "q":"--.-",
+    "r":".-.",
+    "s":"...",
+    "t":"-",
+    "u":"..-",
+    "v":"...-",
+    "w":".--",
+    "x":"-..-",
+    "y":"-.--",
+    "z":"--.."
+
 }
+
 
 def beep(duration):
     frequency = 200  # Set Frequency To 2500 Hertz
-    # duration = 3000  # Set Duration To 1000 ms == 1 second
     winsound.Beep(frequency, duration)
 
 
 def long_beep():
-    beep(3000)
+    beep(1500)
 
 
 def short_beep():
-    beep(1000)
+    beep(500)
 
 
 # define all letters(data struture)
-
 def play_sound_for_letter(letter):
-    print(letter)
+    for sound in letter:
+        if sound != "":
+            sound += MORSE_CODE[letter]
+
+        else:
+            sound += ""
+    
+
+
+
+
      # define all letters
      # look up letters based on input
      # loop over each character in the value string
@@ -31,8 +63,17 @@ def play_sound_for_letter(letter):
 
 # returns single letter (eg: a, b, c) for a given pattern (eg: .-, ---.)
 def get_letter_for_pattern(pattern):
-    print(pattern)
+    morse = ""
+    for message in pattern:
+        if message != "":
+            morse += MORSE_CODE[pattern] + ""
+        else:
+
+            morse += ""
+
 
 
 # long_beep()
-short_beep()
+# short_beep()
+get_letter_for_pattern("h")
+play_sound_for_letter
